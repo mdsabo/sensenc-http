60d34344f0b053aff46075360d68822b7ee1e0dd
slabo101 <slab.o101@gmail.com>
2018-08-19 14:18:19 -0400

    add sqlite and post support

--- server.js
+++ server.js
@@ -1,15 +1,57 @@
 const http = require('http');
-var express = require('express')();
+var app = require('express')();
+var parser = require('body-parser');
+const sqlite3 = require('sqlite3');
 
 const port = process.env.PORT || 5000;
-
-express.get('/', (req, res) => {
-	res.send('Hello from express!');
+ 
+// open database in memory
+let db = new sqlite3.Database(':memory:', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
 });
 
-express.listen(port, (err) => {
+db.run('CREATE TABLE data(sample TEXT)');
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+	res.send('SenseNC heroku webpage!');
+});
+
+app.get('/data', (req, res) => {
+	let sql = `SELECT * from data`;
+	let concat = "";
+	db.each(sql, [], (err, rows) => {
+		if (err) {
+			throw err;
+		}
+		concat += row.string + "|";
+	});
+	res.send(concat);
+});
+
+app.post('/', (req, res) => {
+	db.run(`INSERT INTO data(sample) VALUES(?)`, [req.body.sample], (err) => {
+		if (err) {
+      		return console.log(err.message);
+    	}
+	});
+});
+
+app.listen(port, (err) => {
 	if (err) {
 		return console.log('Server initialization falied.');
 	}
 	console.log(`Server running on ${port}/`);
+});
+
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
 });
